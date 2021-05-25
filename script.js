7b2665fcad57f715dc85c47e64926f533ccc9d91
Enchilada <enchilada@Pavilion.local>
2021-05-25 17:22:02 +0200

    Init

---
+++ script.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById('Canvas');
+
+
+function compileShader(gl, shaderSource, shaderType) {
+    var shader = gl.createShader(shaderType);
+    gl.shaderSource(shader, shaderSource);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (!success) {
+        throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+    }
+    return shader;
+}
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!success) {
+        throw ("program failed to link:" + gl.getProgramInfoLog (program));
+    }   
+    return program;
+  };
