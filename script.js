3ad935d48a50bf9218804045a4dcd90460deb179
RawEnchilada <wittmann.aron@gmail.com>
2021-05-25 19:32:20 +0200

    Test

--- script.js
+++ script.js
@@ -1,24 +1,188 @@
 const canvas = document.getElementById('Canvas');
+const gl = canvas.getContext("webgl");
 
+init();
+
+/*
+#########################################################################################
+#                     ______                _   _                                       #
+#                    |  ____|              | | (_)                                      #
+#                    | |__ _   _ _ __   ___| |_ _  ___  _ __  ___                       #
+#                    |  __| | | | '_ \ / __| __| |/ _ \| '_ \/ __|                      #
+#                    | |  | |_| | | | | (__| |_| | (_) | | | \__ \                      #
+#                    |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/                      #
+#                                                                                       #
+#########################################################################################
+*/
+
+//webgl basics-------------------------------------------------------------------------------------------------------------------
+
+
+function init(){
+    var program = createProgram(gl,
+        createShaderFromScript(gl,"Vert",gl.VERTEX_SHADER),
+        createShaderFromScript(gl,"Frag",gl.FRAGMENT_SHADER));
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    
+    var positions = [
+        0, 0,
+        0, 0.5,
+        0.7, 0,
+      ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.useProgram(program);
+    
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionAttributeLocation, size, type, normalize, stride, offset);
+
+    
+    // draw
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 3;
+    gl.drawArrays(primitiveType, offset, count);    
+}
 
 function compileShader(gl, shaderSource, shaderType) {
-    var shader = gl.createShader(shaderType);
+    let shader = gl.createShader(shaderType);
     gl.shaderSource(shader, shaderSource);
     gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
     if (!success) {
         throw "could not compile shader:" + gl.getShaderInfoLog(shader);
     }
     return shader;
 }
+function createShaderFromScript(gl, scriptId, opt_shaderType) {
+    let shaderScript = document.getElementById(scriptId);
+    if (!shaderScript) {
+        throw ("*** Error: unknown script element" + scriptId);
+    }
+    let shaderSource = shaderScript.text;
+    if (!opt_shaderType) {
+        if (shaderScript.type == "x-shader/x-vertex") {
+            opt_shaderType = gl.VERTEX_SHADER;
+        } else if (shaderScript.type == "x-shader/x-fragment") {
+            opt_shaderType = gl.FRAGMENT_SHADER;
+        } else if (!opt_shaderType) {
+            throw ("*** Error: shader type not set");
+        }
+    }
+
+    return compileShader(gl, shaderSource, opt_shaderType);
+};
 function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
+    let program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    let success = gl.getProgramParameter(program, gl.LINK_STATUS);
     if (!success) {
-        throw ("program failed to link:" + gl.getProgramInfoLog (program));
-    }   
+        throw ("program failed to link:" + gl.getProgramInfoLog(program));
+    }
     return program;
-  };
+};
+
+//FBO-------------------------------------------------------------------------------------------------------------------
+
+function createFBO (w, h, internalFormat, format, type, param) {
+    gl.activeTexture(gl.TEXTURE0);
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, param);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, param);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, w, h, 0, format, type, null);
+
+    let fbo = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+    gl.viewport(0, 0, w, h);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    let texelSizeX = 1.0 / w;
+    let texelSizeY = 1.0 / h;
+
+    return {
+        texture,
+        fbo,
+        width: w,
+        height: h,
+        texelSizeX,
+        texelSizeY,
+        attach (id) {
+            gl.activeTexture(gl.TEXTURE0 + id);
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            return id;
+        }
+    };
+}
+
+function createDoubleFBO (w, h, internalFormat, format, type, param) {
+    let fbo1 = createFBO(w, h, internalFormat, format, type, param);
+    let fbo2 = createFBO(w, h, internalFormat, format, type, param);
+
+    return {
+        width: w,
+        height: h,
+        texelSizeX: fbo1.texelSizeX,
+        texelSizeY: fbo1.texelSizeY,
+        get read () {
+            return fbo1;
+        },
+        set read (value) {
+            fbo1 = value;
+        },
+        get write () {
+            return fbo2;
+        },
+        set write (value) {
+            fbo2 = value;
+        },
+        swap () {
+            let temp = fbo1;
+            fbo1 = fbo2;
+            fbo2 = temp;
+        }
+    }
+}
+
+function resizeFBO (target, w, h, internalFormat, format, type, param) {
+    let newFBO = createFBO(w, h, internalFormat, format, type, param);
+    copyProgram.bind();
+    gl.uniform1i(copyProgram.uniforms.uTexture, target.attach(0));
+    blit(newFBO);
+    return newFBO;
+}
+
+function resizeDoubleFBO (target, w, h, internalFormat, format, type, param) {
+    if (target.width == w && target.height == h)
+        return target;
+    target.read = resizeFBO(target.read, w, h, internalFormat, format, type, param);
+    target.write = createFBO(w, h, internalFormat, format, type, param);
+    target.width = w;
+    target.height = h;
+    target.texelSizeX = 1.0 / w;
+    target.texelSizeY = 1.0 / h;
+    return target;
+}
